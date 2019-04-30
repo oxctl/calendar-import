99dd7d0a0432b8f98a095a4d607d28fa8f1247c2
Matthew Buckett <matthew.buckett@it.ox.ac.uk>
2019-04-30 08:34:05 +0100

    Re-enable the context startup test.

--- src/test/java/uk/ac/ox/it/calendarimporter/CalendarImporterForCanvasApplicationTests.java
+++ src/test/java/uk/ac/ox/it/calendarimporter/CalendarImporterForCanvasApplicationTests.java
@@ -1,9 +1,15 @@
 package uk.ac.ox.it.calendarimporter;
 
-// @RunWith(SpringRunner.class)
-// @SpringBootTest
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class CalendarImporterForCanvasApplicationTests {
 
-  //  @Test
-  public void contextLoads() {}
+    @Test
+    public void contextLoads() {
+    }
 }
