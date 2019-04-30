fab25bdbbce99b8d29399b76cfc174e272b4f5b6
Matthew Buckett <matthew.buckett@it.ox.ac.uk>
2019-04-30 09:30:10 +0100

    Refactored out the client configuration.
    We pull the Canvas client configuration from the properties file now rather than having it hardcoded into the source code.

--- src/main/java/uk/ac/ox/it/calendarimporter/service/TenantClientRegistrationRepository.java
+++ src/main/java/uk/ac/ox/it/calendarimporter/service/TenantClientRegistrationRepository.java
@@ -7,6 +7,7 @@ import org.springframework.security.oauth2.client.registration.ClientRegistratio
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.stereotype.Service;
+import uk.ac.ox.it.calendarimporter.ClientRegistrationConfiguration;
 import uk.ac.ox.it.calendarimporter.persistence.model.Tenant;
 import uk.ac.ox.it.calendarimporter.persistence.repo.TenantRepository;
 
@@ -16,34 +17,31 @@ public class TenantClientRegistrationRepository implements ClientRegistrationRep
 
   @Autowired private TenantRepository tenantRepository;
 
+  @Autowired private ClientRegistrationConfiguration config;
+
   @Override
   public ClientRegistration findByRegistrationId(String registrationId) {
-    // TODO We probably want to make this method cachable or
+    // The repository should be doing the caching so we don't need to do any caching here.
     Optional<Tenant> optionalTenant = tenantRepository.findByName(registrationId);
     if (optionalTenant.isEmpty()) {
       return null;
     }
     Tenant tenant = optionalTenant.get();
 
-    // TODO We should pull this from config
     ClientRegistration registration =
         ClientRegistration.withRegistrationId(tenant.getName())
             .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-            .authorizationUri(tenant.getUrl() + "/login/oauth2/auth")
+            .authorizationUri(tenant.getUrl() + config.getAuthorizationUriPath())
             .clientAuthenticationMethod(ClientAuthenticationMethod.POST)
             .clientId(tenant.getOauth2Id())
             .clientName(tenant.getDisplayName())
             .clientSecret(tenant.getOauth2Secret())
-            .redirectUriTemplate("{baseUrl}/login/oauth2/code/{registrationId}")
-            .tokenUri(tenant.getUrl() + "/login/oauth2/token")
-            .userInfoUri(tenant.getUrl() + "/api/v1/users/self")
-            .userNameAttributeName("login_id")
+            .redirectUriTemplate(config.getRedirectUriTemplate())
+            .tokenUri(tenant.getUrl() + config.getTokenUriPath())
+            .userInfoUri(tenant.getUrl() + config.getUserInfoUriPath())
+            .userNameAttributeName(config.getUserNameAttributeName())
             // You can send scopes and they will be ignored if scoping isn't enabled.
-            .scope(
-                "url:GET|/api/v1/calendar_events",
-                "url:DELETE|/api/v1/calendar_events/:id",
-                "url:POST|/api/v1/calendar_events",
-                "url:GET|/api/v1/courses/:course_id/sections")
+            .scope(config.getScopes())
             .build();
 
     return registration;
