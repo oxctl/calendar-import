e61b026a4a22cc8607183e34f9182d277c8f4912
Matthew Buckett <matthew.buckett@it.ox.ac.uk>
2019-05-01 16:16:25 +0100

    Test the XML that we get back makes sense.

--- src/test/java/uk/ac/ox/it/calendarimporter/controller/ConfigControllerTest.java
+++ src/test/java/uk/ac/ox/it/calendarimporter/controller/ConfigControllerTest.java
@@ -1,7 +1,5 @@
 package uk.ac.ox.it.calendarimporter.controller;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import edu.ksu.lti.launch.service.LtiLoginService;
 import edu.ksu.lti.launch.service.ToolConsumerService;
 import org.junit.Test;
@@ -9,28 +7,47 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.Map;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 @RunWith(SpringRunner.class)
 @WebMvcTest(ConfigController.class)
 public class ConfigControllerTest {
 
-  @Autowired private MockMvc mockMvc;
+    private Map<String, String> namespaces = Map.of(
+            "blti", "http://www.imsglobal.org/xsd/imsbasiclti_v1p0"
+    );
 
-  @MockBean private OAuth2AuthorizedClientRepository oauth2Repository;
+    @Autowired
+    private MockMvc mockMvc;
 
-  @MockBean private ClientRegistrationRepository clientRepository;
+    @MockBean
+    private OAuth2AuthorizedClientRepository oauth2Repository;
 
-  @MockBean private LtiLoginService ltiLoginService;
+    @MockBean
+    private ClientRegistrationRepository clientRepository;
 
-  @MockBean private ToolConsumerService toolConsumerService;
+    @MockBean
+    private LtiLoginService ltiLoginService;
 
-  @Test
-  public void testSimple() throws Exception {
-    mockMvc.perform(MockMvcRequestBuilders.get("/config.xml")).andExpect(status().isOk());
-  }
+    @MockBean
+    private ToolConsumerService toolConsumerService;
+
+    @Test
+    public void testSimple() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/config.xml"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_XML))
+                .andExpect(xpath("//blti:title", namespaces, null).string("Calendar Import"))
+                .andExpect(xpath("//blti:launch_url", namespaces, null).string("http://localhost/launch"))
+        ;
+    }
 }
