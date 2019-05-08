795a4f18d6f6eac4555920197b6af46dd98bdc9a
Matthew Buckett <matthew.buckett@it.ox.ac.uk>
2019-05-08 11:41:14 +0100

    Cleanup indentation.

--- src/main/java/uk/ac/ox/it/calendarimporter/controller/DefaultModelAttributes.java
+++ src/main/java/uk/ac/ox/it/calendarimporter/controller/DefaultModelAttributes.java
@@ -8,49 +8,49 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 @ControllerAdvice
 public class DefaultModelAttributes {
 
-    @Value("${calendar.common.css}")
-    private String defaultCommonCss;
+  @Value("${calendar.common.css}")
+  private String defaultCommonCss;
 
-    @Value("${calendar.brand.json}")
-    private String defaultBrandJson;
+  @Value("${calendar.brand.json}")
+  private String defaultBrandJson;
 
-    @Value("${spring.application.name}")
-    private String applicationName;
+  @Value("${spring.application.name}")
+  private String applicationName;
 
-    @ModelAttribute("canvasCommonCss")
-    public String canvasCommonCss(LtiSession ltiSession) {
-        String canvasCss = null;
-        if (ltiSession != null && ltiSession.getLtiLaunchData() != null) {
-            canvasCss = ltiSession.getLtiLaunchData().getCustom().get("canvas_css_common");
-        }
-        if (canvasCss == null) {
-            canvasCss = defaultCommonCss;
-        }
-        return canvasCss;
+  @ModelAttribute("canvasCommonCss")
+  public String canvasCommonCss(LtiSession ltiSession) {
+    String canvasCss = null;
+    if (ltiSession != null && ltiSession.getLtiLaunchData() != null) {
+      canvasCss = ltiSession.getLtiLaunchData().getCustom().get("canvas_css_common");
     }
-
-    @ModelAttribute("canvasBrandCss")
-    public String canvasBrandCss(LtiSession ltiSession) {
-        String canvasJson = null;
-        String canvasCss = null;
-        if (ltiSession != null && ltiSession.getLtiLaunchData() != null) {
-            canvasJson =
-                    ltiSession.getLtiLaunchData().getCustom().get("com_instructure_brand_config_json_url");
-        }
-        if (canvasJson == null) {
-            canvasJson = defaultBrandJson;
-        }
-        // This is a fudge as at the moment a CSS version exists alongside the JS version.
-        if (canvasJson != null) {
-            if (canvasJson.endsWith(".json")) {
-                canvasCss = canvasJson.substring(0, canvasJson.length() - ".json".length()) + ".css";
-            }
-        }
-        return canvasCss;
+    if (canvasCss == null) {
+      canvasCss = defaultCommonCss;
     }
+    return canvasCss;
+  }
 
-    @ModelAttribute("applicationName")
-    public String applicationName() {
-        return applicationName;
+  @ModelAttribute("canvasBrandCss")
+  public String canvasBrandCss(LtiSession ltiSession) {
+    String canvasJson = null;
+    String canvasCss = null;
+    if (ltiSession != null && ltiSession.getLtiLaunchData() != null) {
+      canvasJson =
+          ltiSession.getLtiLaunchData().getCustom().get("com_instructure_brand_config_json_url");
     }
+    if (canvasJson == null) {
+      canvasJson = defaultBrandJson;
+    }
+    // This is a fudge as at the moment a CSS version exists alongside the JS version.
+    if (canvasJson != null) {
+      if (canvasJson.endsWith(".json")) {
+        canvasCss = canvasJson.substring(0, canvasJson.length() - ".json".length()) + ".css";
+      }
+    }
+    return canvasCss;
+  }
+
+  @ModelAttribute("applicationName")
+  public String applicationName() {
+    return applicationName;
+  }
 }
