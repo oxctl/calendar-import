151a62da9f5085a3c7cee18bfd6b1ad364a0d969
Matthew Buckett <matthew.buckett@it.ox.ac.uk>
2019-10-20 16:24:42 +0100

    Added JavaDoc.

--- src/main/java/uk/ac/ox/it/calendarimporter/persistence/repo/ImportedEventRepository.java
+++ src/main/java/uk/ac/ox/it/calendarimporter/persistence/repo/ImportedEventRepository.java
@@ -5,6 +5,10 @@ import org.springframework.data.repository.CrudRepository;
 import uk.ac.ox.it.calendarimporter.persistence.model.CalendarImport;
 import uk.ac.ox.it.calendarimporter.persistence.model.ImportedEvent;
 
+/**
+ * Stores details of each event imported into a calendar. This allows us to remove all the events
+ * at a later point.
+ */
 public interface ImportedEventRepository
     extends CrudRepository<ImportedEvent, ImportedEvent.ImportedEventIdentity> {
 
