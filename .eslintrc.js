d3c06c450c6dfee5c60bfe24b40c16c82c2e87c8
Matthew Buckett <matthew.buckett@ctl.ox.ac.uk>
2021-07-06 11:25:11 +0100

    CI-81 Update the react-scripts version
    The newer react-scripts version doesn't play well with the eslint config that Instructure supply, specifically the ejs plugin. For now we disable the eslint configuration.
    
    Also update the Instructure UI version.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,3 @@
-module.exports = require('@instructure/ui-eslint-config')
+// This is disabled because the ejs plugin (in the instructure config) causes the build to blow up:
+// https://github.com/jtmthf/eslint-plugin-ejs/issues/1
+// module.exports = require('@instructure/ui-eslint-config')
