d3e19057e4b59ed4da66492e494e758d20768b1a
christopherjohnson <christophersebastianjohnson@gmail.com>
2022-11-09 16:59:22 +0000

    AB#63568 replace puppeteer with playwright

---
+++ deployment/playwright.config.js
@@ -0,0 +1,11 @@
+const config = {
+  use: {
+    // useful for debugging locally
+    // headless: false,
+    // viewport: { width: 1280, height: 720 },
+    // ignoreHTTPSErrors: true,
+    // video: 'on-first-retry',
+  },
+};
+
+module.exports = config;
