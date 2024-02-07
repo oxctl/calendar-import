03509efd3c5a9684ee70af8412d8f3c84ed2d742
christopherjohnson <christophersebastianjohnson@gmail.com>
2024-02-07 10:01:46 +0000

    AB#73274 HLR_22: read credentials from AWS Secrets Manager  * up to spring boot 3

--- backend/src/test/java/uk/ac/ox/it/calendarimporter/persistence/repo/TenantRepositoryTest.java
+++ backend/src/test/java/uk/ac/ox/it/calendarimporter/persistence/repo/TenantRepositoryTest.java
@@ -6,7 +6,6 @@ import org.opentest4j.AssertionFailedError;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-//import jakarta.validation.ConstraintViolationException;
 import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.test.annotation.DirtiesContext;
 import uk.ac.ox.it.calendarimporter.persistence.model.Tenant;
