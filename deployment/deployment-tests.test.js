9f75a5c2ceb2853985ff15e730e6a02cb883f021
christopherjohnson <christophersebastianjohnson@gmail.com>
2025-03-07 17:07:09 +0000

    refactor deployment tests

--- deployment/deployment-tests.test.js
+++ deployment/deployment-tests.test.js
@@ -1,72 +1,69 @@
-const { test } = require('@playwright/test')
-const assert = require('assert')
-require('dotenv').config()
+import { test, expect } from '@playwright/test'
+import { goToTool, addNetworkThrottle, NETWORK_PRESETS, screenshot, getToolAnchor } from './test-utils'
+import { networkPreset } from './constants'
+import path from 'path'
+test('Calendar import deployment tests', async ({context, page}, testInfo) => {
 
-const token = process.env.OAUTH_TOKEN
-const host = process.env.CANVAS_HOST
-const courseId = process.env.COURSE_ID
-const toolId = process.env.TOOL_ID
+  let ltiToolFrame
 
-test.beforeAll(async () => {
-  assert(token, 'You must set the environmental variable OAUTH_TOKEN')
-  assert(host, 'You must set the environmental variable CANVAS_HOST')
-  assert(courseId, 'You must set the environmental variable COURSE_ID')
-  assert(toolId, 'You must set the environmental variable TOOL_ID')
-})
+  await test.step('Load the tool', async () => {
+    if (networkPreset) {
+      await addNetworkThrottle(context, page, NETWORK_PRESETS[networkPreset])
+    }
+    ltiToolFrame = await goToTool(page)
+    if (page.url().includes('beta')) {
+      // the warning banner on beta interferes with some click operations so dismiss it
+      await page.getByRole('button', {name: 'Close warning'}).click()
+    }
+  })
 
-const login = async (request, page) => {
-  await Promise.all([
-    await request.get(`${host}/login/session_token`, {
-      headers: {
-        ContentType: 'application/json',
-        Authorization: `Bearer ${token}`
-      }
-    }).then(async (response) => {
-      const json = await response.json()
-      const sessionUrl = json.session_url
-      return page.goto(sessionUrl)
-    })
-  ])
-}
+  await test.step('Tool loads', async () => {
+    const title = await getToolAnchor(ltiToolFrame)
+    await expect(title).toBeVisible()
+  })
 
-const grantAccess = async (context, frameLocator) => {
-  const button = await frameLocator.getByRole('button')
-  const [newPage] = await Promise.all([
-    context.waitForEvent('page'),
-    await button.click()
-  ])
+  await test.step('Shows imports or no imports', async () => {
+    const imports = ltiToolFrame.getByText(/.* imported .* into the/)
+    const noImports = ltiToolFrame.getByText("No previous imports found")
+    await expect(imports.first().or(noImports)).toBeVisible();
+  })
 
-  const submit = await newPage.getByRole('button', {name: /Authori[sz]e/})
-  await submit.click()
-  const close = await newPage.getByText('Close', {exact: true})
-  await close.click()
-}
 
-test('Calendar import loads and shows the previous imports or that there are none.', async ({ context, page, request }) => {
-  await login(request, page)
-  await page.goto(`${host}/courses/${courseId}/external_tools/${toolId}?launch_type=course_home_sub_navigation`)
-  // the frame id now seems to be dynamic on beta, but fixed on live - this should allow it to work for both cases
-  const frames = page.frames();
-  const frameLocator = frames.find((frame) => frame.name().includes('tool_content'));
+  await test.step('Course and section are selectable', async () => {
+    const sectionCombo = ltiToolFrame.getByRole('combobox')
+    await sectionCombo.click()
+    const listOpts = ltiToolFrame.getByRole('listbox')
+    await expect(listOpts).toContainText('Course: Deployment Testing')
+    await expect(listOpts).toContainText('Section: Deployment Testing')
 
-  const needsGrantAccess = await Promise.race([
-    frameLocator.getByText('Please Grant Access').waitFor()
-        .then(() => { return true } ),
-    frameLocator.getByLabel('Section', {exact: true}).waitFor()
-        .then(() => { return false } )
-  ])
+    const courseOpt = listOpts.getByRole('option', {name: 'Course: Deployment Testing'})
+    await expect(courseOpt).toHaveCount(1)
+    await expect(courseOpt).toBeEnabled()
 
-  if(needsGrantAccess){
-    await grantAccess(context, frameLocator)
-  }
+    const sectionOpt = listOpts.getByRole('option', {name: 'Section: Deployment Testing'})
+    await expect(sectionOpt).toHaveCount(1)
+    await expect(sectionOpt).toBeEnabled()
+  })
 
-  // We could shortcut this if we see an error message, but I think it's reasonable to just wait on the timeout
-  // instead of complicating the code.
-  
-  // Wait until we either load an import or find that there's no imports in the course.
-  // These requests depend on the backend so tests both the backend and the frontend availability.
-  await Promise.race([
-      frameLocator.getByText(/.* imported .* into the/).first().waitFor(),
-      frameLocator.getByText("No previous imports found").waitFor(),
-  ])
+  let importButton
+  await test.step('Import without file shows warning', async () => {
+    importButton = ltiToolFrame.getByRole('button', {name: 'Import'})
+    await importButton.click()
+    await expect(ltiToolFrame.getByText('You must provide a file')).toBeVisible()
+  })
+
+  await test.step('Can upload a file and upload starts', async () => {
+    const fileChooserPromise = page.waitForEvent('filechooser')
+    await ltiToolFrame.getByText('browse your files').click()
+    const fileChooser = await fileChooserPromise
+    await fileChooser.setFiles(path.join(__dirname, 'resources', 'example.csv'))
+    await expect(ltiToolFrame.getByText('Selected: example.csv')).toBeVisible()
+    await importButton.click()
+
+    await expect(ltiToolFrame.getByText('Calendar import started, click "Reload" button to follow its progress.')).toBeVisible();
+
+    // TODO: ideally we would test for Import Completed
+    //  but complicated as there will be an audit of all previous imports an no date on Import: Completed
+    //  We should be able use the purge endpoint to clear out the previous imports
+  })
 })
