c25f0364a381fe3df1277d12943380af7ae7a796
christopherjohnson <christophersebastianjohnson@gmail.com>
2025-03-11 16:49:02 +0000

    deployment tests - "Import File" title is visible before authentication so tests failed - replace with "Section" which waits until authentication or load

--- deployment/constants.js
+++ deployment/constants.js
@@ -7,6 +7,6 @@ export const toolId = process.env.TOOL_ID
 export const courseId = 186641
 export const networkPreset = process.env.NETWORK_PRESET
 export const toolAnchorMethod = 'getByText'
-export const toolAnchorText = 'Import File'
+export const toolAnchorText = 'Section'
 export const toolAnchorOptions = { exact: true }
 export const toolUrl = `${host}/courses/${courseId}/external_tools/${toolId}?launch_type=course_home_sub_navigation`
